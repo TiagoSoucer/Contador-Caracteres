e07661024878d949279eaf3ed4de6aae9fa70f23
Tiago Souza <74255959+TiagoSoucer@users.noreply.github.com>
2022-10-26 15:50:38 -0300

    Add files via upload

---
+++ ContCaractere.js
@@ -0,0 +1,10 @@
+ function countText() {
+    let text = document.form_main.text.value;
+
+    document.getElementById('characteres').innerText = text.length
+
+    document.getElementById('words').innerText = text.length == 0 ? 0 : text.split(/\s+/).length;
+
+    document.getElementById('rows').innerText = text.length == 0 ? 0 : text.split(/\n/).length;
+}
+
